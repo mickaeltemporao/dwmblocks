2eb84e808346aa5d1c2dd24b8247d4a05f446362
Hamza Mughal <hamza@prodesquare.com>
2020-08-05 17:38:09 +0500

    updated location of statusbar scripts

--- blocks.h
+++ blocks.h
@@ -1,12 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    {"  ", "~/bin/statusbar/blevel",					0,		11},
-	{" ",  "~/bin/statusbar/volume",						0,		10},
-	{" ",  "~/bin/statusbar/battery",						2,		0},
-	{" ",  "~/bin/statusbar/clock",						5,		0},
-	{"  ", "~/bin/statusbar/cpu",				5,		0},
-	{" ",  "~/bin/statusbar/internet",				5,		0},
+	{"  ", "~/.local/bin/statusbar/blevel",					0,		11},
+	{" ",  "~/.local/bin/statusbar/volume",						0,		10},
+	{" ",  "~/.local/bin/statusbar/battery",						2,		0},
+	{" ",  "~/.local/bin/statusbar/clock",						5,		0},
+	{"  ", "~/.local/bin/statusbar/cpu",				5,		0},
+	{" ",  "~/.local/bin/statusbar/internet",				5,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
