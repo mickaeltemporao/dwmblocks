0d7b21b74ed402e60100fc53bbd2a63168fe81cb
Mickael Temporão <16172142+mickaeltemporao@users.noreply.github.com>
2020-12-19 20:06:45 +0100

    switch layout

--- blocks.h
+++ blocks.h
@@ -1,13 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"  ", "~/.local/bin/statusbar/blevel",					0,		11},
-	{" ",  "~/.local/bin/statusbar/volume",						0,		10},
-	{" ",  "~/.local/bin/statusbar/battery",						2,		0},
-	{" ",  "~/.local/bin/statusbar/clock",						5,		0},
+	/* {"  ", "~/.local/bin/statusbar/blevel",					0,		11}, */
+	/* {" ",  "~/.local/bin/statusbar/volume",						0,		10}, */
 	{"  ", "~/.local/bin/statusbar/cpu",				5,		0},
 	{" ",  "~/.local/bin/statusbar/internet",				5,		0},
+	{" ",  "~/.local/bin/statusbar/battery",						2,		0},
+	{" ",  "~/.local/bin/statusbar/clock",						5,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim = '/';
+static char delim = '|';
